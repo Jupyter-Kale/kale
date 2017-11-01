b623b77e174b7c54d4aea78ab96ce43ce960c205
Oliver Evans <oevans@lbl.gov>
2017-11-01 10:49:57 -0700

    Add setup, requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='kale_workflows',
+    version='0.1',
+    description='Jupyter HPC Workflows',
+    package_dir= {'kale_workflows': 'lib'},
+    packages={'kale_workflows'}
+    )
